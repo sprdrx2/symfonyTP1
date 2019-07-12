0cacd68002d0c9a98bf30e6b14e8e24c827b03c4
Saami PERDRIX <saami.perdrix@cgi.com>
2019-07-12 10:05:23 +0200

    corr. precedent commit (redirection correcte apres ajout produit depuis page manager)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -70,7 +70,7 @@ class ProductController extends AbstractController
 	    $entityManager->persist($product);
             $entityManager->flush();
 
-            return $this->redirectToRoute('product_index');
+            return $this->redirectToRoute('rayon_manager_office');
         }
 
         return $this->render('product/new.html.twig', [
